7e43529942acce2eeee95eb7ae43c9b58c515e62
Philip Vanloo <pva@velleman.be>
2023-08-21 15:32:19 +0200

    Update roundDecimals, auctionTime Format IMPORT_CONFIG

--- dkp.js
+++ dkp.js
@@ -1,40 +1,43 @@
 const IMPORT_CONFIG = {
-    "itemValueMode":1,
-    "allowEqualMax":false,
-    "raidCompletionBonusValue":0,
-    "autoAwardOnlineOnly":false,
-    "dynamicValue":false,
-    "zeroSumBank":false,
-    "autoAwardSameZoneOnly":false,
-    "minimalIncrement":1,
-    "weeklyReset":0,
-    "onTimeBonus":false,
-    "intervalBonusValue":0,
-    "onTimeBonusValue":0,
-    "minimumPoints":0,
-    "useOS":true,
-    "antiSnipe":0,
-    "benchMultiplier":1,
-    "selfBenchSubscribe":false,
-    "baseAlways":false,
-    "auctionTime":45,
-    "raidCompletionBonus":false,
-    "minGP":1,"intervalBonusTime":0,
-    "hardCap":0,"autoAwardIncludeBench":true,
-    "allowBelowMinStandings":false,"weeklyCap":0,
-    "bossKillBonus":false,
-    "intervalBonus":false,
-    "allowCancelPass":false,
-    "allInAlways":false,
-    "zeroSumBankInflation":0,
-    "roundPR":10,
-    "namedButtons":false,
-    "bossKillBonusValue":0,
-    "autoBenchLeavers":false,
-    "auctionType":2,
-    "rollTime":20,
-    "tax":0,
-    "roundDecimals":0
+    "itemValueMode": 1,
+    "allowEqualMax": false,
+    "raidCompletionBonusValue": 0,
+    "autoAwardOnlineOnly": false,
+    "dynamicValue": false,
+    "zeroSumBank": false,
+    "autoAwardSameZoneOnly": false,
+    "minimalIncrement": 1,
+    "weeklyReset": 0,
+    "onTimeBonus": false,
+    "intervalBonusValue": 0,
+    "onTimeBonusValue": 0,
+    "minimumPoints": 0,
+    "useOS": true,
+    "antiSnipe": 0,
+    "benchMultiplier": 1,
+    "selfBenchSubscribe": false,
+    "baseAlways": false,
+    "auctionTime": 120,
+    "raidCompletionBonus": false,
+    "minGP": 1,
+    "intervalBonusTime": 0,
+    "hardCap": 0,
+    "autoAwardIncludeBench": true,
+    "allowBelowMinStandings": false,
+    "weeklyCap": 0,
+    "bossKillBonus": false,
+    "intervalBonus": false,
+    "allowCancelPass": false,
+    "allInAlways": false,
+    "zeroSumBankInflation": 0,
+    "roundPR": 10,
+    "namedButtons": false,
+    "bossKillBonusValue": 0,
+    "autoBenchLeavers": false,
+    "auctionType": 2,
+    "rollTime": 20,
+    "tax": 0,
+    "roundDecimals": 2
 };
 
 function doWork() {
@@ -49,22 +52,7 @@ function doWork() {
 
 function doImport() {
     let textarea = document.getElementById("import-dkp-textarea");
-
     let players = [];
-    let importObject = {
-        standings: {
-            roster: [{
-                    uid: 1662751134,
-                    name: "Hardcore",
-                    fieldNames: [],
-                    standings: {
-                        player: players
-                    },
-                    config: IMPORT_CONFIG
-                }
-            ]
-        }
-    };
 
     textarea.value.split("\n").forEach(player => {
         [pName, pClass, pRole, pDkp] = player.split("\t");
@@ -79,9 +67,22 @@ function doImport() {
         });
     });
 
-    textarea.value = JSON.stringify(importObject);
+    textarea.value = JSON.stringify({
+        standings: {
+            roster: [{
+                    uid: 1662751134,
+                    name: "Hardcore",
+                    fieldNames: [],
+                    standings: {
+                        player: players
+                    },
+                    config: IMPORT_CONFIG
+                }
+            ]
+        }
+    });
 }
 
 function doExport() {
-    console.log("export");
+    let textarea = document.getElementById("export-loot-textarea");
 }
